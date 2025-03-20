b91ce2dd9b55be1714fdad83c4d367608a29fdfa
example <email@example.cgit config --global user.name>
2025-03-20 23:59:12 +0300

    Login Page

---
+++ Login/Pages/Login.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+            // You can add logic here if needed
+        }
+    }
+}
