257e817aac161420914749e5a56412933eb0f86b
example <email@example.cgit config --global user.name>
2025-03-26 04:44:57 +0300

    Login Page

--- Login/obj/Debug/net9.0/Login.AssemblyInfo.cs
+++ Login/obj/Debug/net9.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b07ea2459d3f9070f0ce0ff55e4af06867a5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91ce2dd9b55be1714fdad83c4d367608a29fdfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
